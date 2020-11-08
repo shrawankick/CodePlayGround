aecd95aa35d158fed85da309d440e6854c8bc68f
Suresh Pragada <42589062+spragadalyft@users.noreply.github.com>
2020-11-07 20:32:16 -0800

    Readability changes

--- CodePlayGround/CodePlayGround/practicePrograms/PrimeNumber.cs
+++ CodePlayGround/CodePlayGround/practicePrograms/PrimeNumber.cs
@@ -6,28 +6,23 @@ using System.Threading.Tasks;
 
 namespace CodePlayGround.practicePrograms
 {
-    class PrimeNumber
-    {
-        public void VerifythenumberisPrime()
-        {
+    class PrimeNumber{
+        public void VerifythenumberisPrime(){
             Console.WriteLine("Enter your number");
             int input = Convert.ToInt32(Console.ReadLine());
-            //int input = 12;
-            int flag = 0;
-            for (int i = 2; i < input / 2; i++)
-            {
-                if (input % i == 0)
-                {
-                    Console.WriteLine(input + " is Not Prime");
-                    flag = 1;
+            boolean isPrime = true;
+            for (int i = 2; i < input / 2; i++){
+                if (input % i == 0){
+                    isPrime = false;
                     break;
                 }
             }
-            if (flag == 0)
-            {
-                Console.WriteLine(input + "is Prime Number");
-            }
             
+            if (isPrime){
+                Console.WriteLine(input + "is Prime Number");
+            } else {
+                Console.WriteLine(input + "is not a Prime Number");
+            }
         }
     }
 }

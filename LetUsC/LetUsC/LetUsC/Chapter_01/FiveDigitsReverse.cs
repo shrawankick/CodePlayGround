7d357fffd85124fd33e8a65019138eb8fc7c144e
shrawan <shrawankick@gmail.com>
2020-11-17 22:53:19 +0530

    just code refactoring removed spacess

--- LetUsC/LetUsC/LetUsC/Chapter_01/FiveDigitsReverse.cs
+++ LetUsC/LetUsC/LetUsC/Chapter_01/FiveDigitsReverse.cs
@@ -8,32 +8,30 @@ namespace LetUsC.Chapter_01
 {
    static  class FiveDigitsReverse
     {
-        public static void Test(this Chapter_01.SumOfFiveDigitNumber number)
-        {
+        public static void Test(this Chapter_01.SumOfFiveDigitNumber number){
             Console.WriteLine("Method Name: extension method");
         }
-
+        /// <summary>
+        /// If a five-digit number is input through the keyboard, 
+        /// write a program to reverse the number.
+        /// </summary>
+        /// <param name="number"></param>
         public static void ReverseOfFiveDigitNumber(this Chapter_01.SumOfFiveDigitNumber number)
         {
             Console.WriteLine("Enter Five digit Number");
             int inputnumber = Convert.ToInt32(Console.ReadLine());
             int refinput = inputnumber;
-            if (inputnumber >= 100000)
-            {
-                Console.WriteLine("enter 5 digits only2345 ");
+            if (inputnumber >= 100000){
+                Console.WriteLine("please enter 5 digits only ");
             }
-            else
-            {
+            else{
                 int rev_num = 0;
-
-                while (inputnumber > 0)
-                {
-                    rev_num = rev_num * 10 + inputnumber % 10;
+                while (inputnumber > 0) {
+                    rev_num = rev_num * 10 + (inputnumber % 10);
                     inputnumber = inputnumber / 10;
                 }
                 Console.WriteLine($"Reverse of the {refinput} is {rev_num}");
             }
-
         }
     }
 }
--- LetUsC/LetUsC/LetUsC/Program.cs
+++ LetUsC/LetUsC/LetUsC/Program.cs
@@ -27,25 +27,18 @@ namespace LetUsC
             // Chapter_01.Gross gross = new Gross();
             //gross.FindGrossSalaryofUser();
             //Chapter_01.DistanceBetweenTwoCitiesInDiffrentFormats
-            //    distanceBetweenTwoCities = new DistanceBetweenTwoCitiesInDiffrentFormats();
+            //distanceBetweenTwoCities = new DistanceBetweenTwoCitiesInDiffrentFormats();
             //distanceBetweenTwoCities.DistanceBetweenTwoCities();
-            //lines();
             //Chapter_01.marks_obtained_by marks_Obtained_By = new marks_obtained_by();
             //marks_Obtained_By.MarksObtainedByAStudentInFiveDifferentSubjects();
-            //lines();
-            //  Chapter_01.Temperature_of_a_city temperature_Conversion = new Temperature_of_a_city();
+            //Chapter_01.Temperature_of_a_city temperature_Conversion = new Temperature_of_a_city();
             // temperature_Conversion.ConvertTempature();
-
-            //lines();
             //Chapter_01.Rectangle_Circle rectangle_Circle = new Rectangle_Circle();
             //rectangle_Circle.RectangleOrCircle();
-
             //Chapter_01.InterchangeTheContents interchangeTheContents = new InterchangeTheContents();
             //interchangeTheContents.InterchangeTheContentsOfTwoNumbers();
-
-
             SumOfFiveDigitNumber ExtensionMethode = new SumOfFiveDigitNumber();
-            // sumOfFiveDigitNumber.Sumoffivedigits();
+            //sumOfFiveDigitNumber.Sumoffivedigits();
             //ExtensionMethode.Test();
             ExtensionMethode.ReverseOfFiveDigitNumber();
         }

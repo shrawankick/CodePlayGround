c6d7ef02ebbb1ef258a588142a2bd9102dfe8c7a
shrawan <shrawankick@gmail.com>
2021-02-12 22:25:16 +0530

    plan to learn

--- LetUsC/LetUsC/LetUsC/Chapter_03/Class1.cs
+++ LetUsC/LetUsC/LetUsC/Chapter_03/Class1.cs
@@ -8,5 +8,19 @@ namespace LetUsC.Chapter_03
 {
     class Class1
     {
+
+//        Code to sot the array(no built functions to be used)
+
+//-          Code to reverse a string
+
+//-          Code to print substrings for a given string.
+
+//-          Code to print repetitive characters in a given string and their count.
+
+//-          Inheritance , Polymorphism , Interface
+
+//-          Selenium Web code  (get the web elements from page based on the search criteria).
+
+//-          Handling excel files.
     }
 }
